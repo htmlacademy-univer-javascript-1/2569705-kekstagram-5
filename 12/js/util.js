62a749db03b4b59285c76e3444a61c4a17beee67
Keks <keks@htmlacademy.ru>
2024-12-28 18:08:22 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/util.js
@@ -0,0 +1,107 @@
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+function createRandomNumber (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+function createNumbersMassive (min, max) {
+  const numbersMassive = [];
+
+  for(let i = min; i <= max; i++) {
+    numbersMassive.push(i);
+  }
+  return numbersMassive;
+}
+
+const closeOnEscKeyDown = (evt, closeFunction) => {
+  if (evt.key === 'Escape') {
+    closeFunction();
+  }
+};
+
+const onModalKeydown = (evt) => {
+  closeOnEscKeyDown(evt, deleteResultMessage());
+};
+
+const onModalButtonClick = () => {
+  deleteResultMessage();
+};
+
+const awayModalClick = (evt) => {
+  if (evt.target === document.body.lastElementChild) {
+    deleteResultMessage();
+  }
+};
+
+function deleteResultMessage () {
+  const addedMessage = document.body.lastElementChild;
+  addedMessage.querySelector('button').removeEventListener('click', onModalButtonClick);
+  document.removeEventListener('keydown', onModalKeydown);
+  document.removeEventListener('click', awayModalClick);
+  addedMessage.remove();
+
+}
+
+const alertLoadError = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '20px';
+  messageAlert.style.backgroundColor = 'rgb(60, 54, 20)';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = 'Ошибка подключения к серверу';
+  messageAlert.style.lineHeight = '35px';
+  document.body.append(messageAlert);
+};
+
+const showModal = (templateId) => {
+  const messageTemplate = document.querySelector(`#${templateId}`).content;
+  const message = messageTemplate.cloneNode(true);
+  const messageFragment = document.createDocumentFragment();
+  messageFragment.appendChild(message);
+  document.body.appendChild(messageFragment);
+};
+
+const showResultMessage = (templateId) => {
+  showModal(templateId);
+  const btn = document.querySelector(`.${templateId}__button`);
+  document.addEventListener('keydown', onModalKeydown);
+  document.addEventListener('click', awayModalClick);
+  btn.addEventListener('click', onModalButtonClick);
+};
+
+const showUploadSucccessMessage = () => showResultMessage('success');
+const showUploadErrorMessage = () => showResultMessage('error');
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export {getRandomInteger, getRandomArrayElement, createRandomNumber, closeOnEscKeyDown, createNumbersMassive, alertLoadError, showUploadErrorMessage, showUploadSucccessMessage, debounce, shuffleArray };
